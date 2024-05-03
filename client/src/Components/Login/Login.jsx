f20db2e43468f379ea012c31603530edf0484a85
bram.van.doren <bramvandoren1@hotmail.com>
2024-05-03 23:49:28 +0200

    aanmaken login form met beetje styling

--- client/src/Components/Login/Login.jsx
+++ client/src/Components/Login/Login.jsx
@@ -1,8 +1,36 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import './Login.css'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div>
+        <h1>Login</h1>
+        <div className=''></div>
+        <a href="/register">To Register</a>
+        <a href="/dashboard">To Dashboard</a>
+        <div class="login-container">
+            <div class="container">
+                <div class="image-container">
+                    <img src='' alt='login-image' />
+                </div>
+            </div>
+            <div class="login-flow">
+                <form class="login-form">
+                <h2>Welkom terug!</h2>
+                <div class="input-group">
+                    <label for="username">E-mail</label>
+                    <input type="text" id="username" name="username" required/>
+                </div>
+                <div class="input-group">
+                    <label for="password">Wachtwoord</label>
+                    <input type="password" id="password" name="password" required/>
+                </div>
+                <button type="submit">Login</button>
+                </form>
+            </div>
+        </div>
+    </div>
   )
 }
 
