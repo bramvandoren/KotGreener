8df91130aa4aaf770b04eee3f7ca3cfa8cf9800d
bram.van.doren <bramvandoren1@hotmail.com>
2024-06-17 04:48:32 +0200

    toevoegen van scroller voor top pagina te gaan

---
+++ client/src/Components/Scroller/ScrollToTop.jsx
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from 'react';
+import './ScrollToTop.css'; // Vergeet niet je CSS bestand te koppelen
+
+const ScrollToTop = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  // Show button when page is scrolled up to given distance
+  const toggleVisibility = () => {
+    if (window.pageYOffset > 300) {
+      setIsVisible(true);
+    } else {
+      setIsVisible(false);
+    }
+  };
+
+  // Set the scroll position to the top of the page
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', toggleVisibility);
+    return () => window.removeEventListener('scroll', toggleVisibility);
+  }, []);
+
+  return (
+    <div className="scroll-to-top">
+      {isVisible && (
+        <div onClick={scrollToTop}>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
+            <path d="M16 22a2 2 0 0 1-1.41-.59l-10-10a2 2 0 0 1 2.82-2.82L16 17.17l8.59-8.58a2 2 0 0 1 2.82 2.82l-10 10A2 2 0 0 1 16 22Z">
+            </path>
+        </svg>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default ScrollToTop;
